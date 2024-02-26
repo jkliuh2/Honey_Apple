86bddcfb62f239fc47726a51a00cae5acf80f36f
TestDesktop <test@test.com>
2024-02-26 18:41:37 +0900

    jkliuh2/Honey_Apple#36 검색페이지 구현중.

--- src/main/java/com/honeyapple/MainPageController.java
+++ src/main/java/com/honeyapple/MainPageController.java
@@ -35,4 +35,28 @@ public class MainPageController {
 		model.addAttribute("articleList", articleList);
 		return "template/layout";
 	}
+	
+	
+	// 더보기+검색 페이지
+	@GetMapping("/honey-apple/search-view")
+	public String searchView(Model model) {
+		
+		// List<Article> 가져오기(임시)
+		List<Article> articleList = articleBO.getArticleList();
+		
+		// 응답값
+		model.addAttribute("viewName", "mainpage/searchView");
+		model.addAttribute("titleName", "검색");
+		model.addAttribute("nav", "검색");
+		model.addAttribute("articleList", articleList);
+		return "template/layout";
+	}
+	
+	// 테스트용 페이지
+	@GetMapping("/test")
+	public String test(Model model) {
+		model.addAttribute("viewName", "test/test");
+		model.addAttribute("titleName", "테스트");
+		return "template/layout";
+	}
 }
