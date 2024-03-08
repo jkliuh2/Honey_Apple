22a3a7a90fd8f0a2e914b55fbfeb50446e75f176
TestDesktop <test@test.com>
2024-03-08 18:49:16 +0900

    리눅스 오류때문에 변경. 안쓰는 이미지 밑 컨트롤러 삭제

--- src/main/java/com/honeyapple/HoneyappleApplication.java
+++ src/main/java/com/honeyapple/HoneyappleApplication.java
@@ -2,11 +2,12 @@ package com.honeyapple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class HoneyappleApplication {
+public class HoneyappleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoneyappleApplication.class, args);
